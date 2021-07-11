c2b430a7fda3827948e4abb443de949703d11d49
Param1203 <86376868+Param1203@users.noreply.github.com>
2021-07-11 11:49:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var student1;
+
+function setup() {
+  createCanvas(400,400);
+  student1 = new Student("Jagoo",14,"Nineth Grade");
+  student1.show();
+}
+function draw() 
+{
+  background(30);
+}
---
+++ student.js
@@ -0,0 +1,14 @@
+
+class Student
+{
+   constructor(n,a,g){
+       this.name = n;
+       this.age = a;
+       this.grade = g;
+   }
+   show(){
+       console.log(this.name);
+       console.log(this.age);
+       console.log(this.grade);
+   }
+}
